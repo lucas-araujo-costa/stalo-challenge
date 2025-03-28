b22dfcd8ab42ad45b9f0c0f21a768886bedcb067
Lucas <lucasaraujo1964@gmail.com>
2025-03-28 13:14:48 -0300

    fix: edit auth user in middleware

--- app/Http/Middleware/EnsureApiAuthentication.php
+++ app/Http/Middleware/EnsureApiAuthentication.php
@@ -4,14 +4,19 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class EnsureApiAuthentication
 {
     public function handle(Request $request, Closure $next)
     {
-        if (!auth('sanctum')->check()) {
+        $user = auth('sanctum')->user();
+
+        if (!$user) {
             return response()->json(['error' => 'Unauthenticated'], 401);
         }
+
+        Auth::setUser($user);
 
         return $next($request);
     }
